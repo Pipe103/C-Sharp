1e4354ddc98a6fc18ce7002857cc4ac781ebdb50
felipe jimenez sanchez <felipe>
2026-02-18 18:16:58 -0500

    Taller de control condicional v2
    actualizacion de taller de control de condicionales

---
+++ retoSistemaClinicaVeterinaria/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace retoSistemaClinicaVeterinaria;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        String especieMacota,motivoConsulta;
+        int edadMacota;
+        bool tieneVacuna;
+
+
+        Console.WriteLine("ingresa tu nombre");
+        String name = Console.ReadLine();
+        Console.WriteLine($"hola" + name);
+    }
+}
--- tallerControlCondicional/Program.cs
+++ tallerControlCondicional/Program.cs
@@ -1,19 +1,17 @@
-﻿namespace tallerControlCondicional;
-//TALLER DE CONTROL CONDICIONAL
-class Program
+﻿//TALLER DE CONTROL CONDICIONAL
+int CheckNumber;
+Console.WriteLine("Positivo, Negativo o Igual a 0");
+Console.WriteLine("ingresa tu numero");
+CheckNumber = int.Parse(Console.ReadLine());
+if (CheckNumber > 0)
 {
-    static void Main(string[] args)
-    {
-        int CheckNumber = 8;
-        if (CheckNumber>0){
-            Console.WriteLine("el numeroes positivo");
-        }else if (CheckNumber < 0)
-        {
-            Console.WriteLine("el numero es negativo");
-        }
-        else
-        {
-         Console.WriteLine("el numero es 0");
-        }
-    }
+    Console.WriteLine("el numeroes positivo");
+}
+else if (CheckNumber < 0)
+{
+    Console.WriteLine("el numero es negativo");
+}
+else
+{
+    Console.WriteLine("el numero es 0");
 }
